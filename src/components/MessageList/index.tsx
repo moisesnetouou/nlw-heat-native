709cc0b4898cd2754d46c395751e1db325d46135
Moisés Neto <moisesnetored@gmail.com>
2021-10-21 15:51:50 -0400

    feat: Mensagens em tempo real com Socket.io

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { ScrollView } from "react-native";
+import { io } from "socket.io-client";
+
 import { api } from "../../services/api";
 
 import { Message, MessageProps } from "../Message";
 import { styles } from "./styles";
+
+let messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+socket.on("new_message", (newMessage) => {
+  messagesQueue.push(newMessage);
+  console.log(newMessage);
+});
 
 export function MessageList() {
   const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
@@ -14,6 +24,21 @@ export function MessageList() {
       setCurrentMessages(messagesResponse.data);
     }
     fetchMessages();
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setCurrentMessages((prevState) => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1],
+        ]);
+        messagesQueue.shift();
+      }
+    }, 3000);
+
+    return () => clearInterval(timer);
   }, []);
 
   return (
