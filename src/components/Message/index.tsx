3cb1b1fedaac3b3b3ef20584ac5f9d73abb94ed0
Moisés Neto <moisesnetored@gmail.com>
2021-10-21 11:48:30 -0400

    feat: Moti inserido ao projeto, Animação com Moti criada

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { MotiView } from "moti";
 import { UserPhoto } from "../UserPhoto";
 
 import { styles } from "./styles";
@@ -19,7 +20,12 @@ type Props = {
 
 export function Message({ data }: Props) {
   return (
-    <View style={styles.container}>
+    <MotiView
+      from={{ opacity: 0, translateY: -50 }}
+      animate={{ opacity: 1, translateY: 0 }}
+      transition={{ type: "timing", duration: 700 }}
+      style={styles.container}
+    >
       <Text style={styles.message}>{data.text}</Text>
 
       <View style={styles.footer}>
@@ -27,6 +33,6 @@ export function Message({ data }: Props) {
 
         <Text style={styles.userName}>{data.user.name}</Text>
       </View>
-    </View>
+    </MotiView>
   );
 }
